309aff914002d7ed235122ce9de4a3a2d5e8980c
Kostya-Bog <59747320+Kostya-Bog@users.noreply.github.com>
2021-01-21 17:28:43 +0200

    Updated Employee.cs

--- TestTask/Abstractions/Employee.cs
+++ TestTask/Abstractions/Employee.cs
@@ -1,20 +1,20 @@
 ﻿namespace TestTask.Abstractions
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Text;
+    using Enums;
 
     public abstract class Employee
     {
+        protected readonly Position position;
         private readonly string fullName;
 
         public string FullName { get => this.fullName; }
         public double Еxperience { get; set; }
 
-        public Employee(string fullName, double experience)
+        public Employee(string fullName, double experience, Position position)
         {
             this.fullName = fullName;
             Еxperience = experience;
+            this.position = position;
         }
 
         public abstract void Work();
