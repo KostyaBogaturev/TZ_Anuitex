92a62bc6ea2993d3e9f578730e1d22acf1219475
Kostya-Bog <59747320+Kostya-Bog@users.noreply.github.com>
2021-01-21 17:02:41 +0200

    Added abstract class Emploee

---
+++ TestTask/Abstractions/Employee.cs
@@ -0,0 +1,23 @@
+﻿namespace TestTask.Abstractions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public abstract class Employee
+    {
+        private readonly string fullName;
+
+        public string FullName { get => this.fullName; }
+        public double Еxperience { get; set; }
+
+        public Employee(string fullName, double experience)
+        {
+            this.fullName = fullName;
+            Еxperience = experience;
+        }
+
+        public abstract void Work();
+
+    }
+}
