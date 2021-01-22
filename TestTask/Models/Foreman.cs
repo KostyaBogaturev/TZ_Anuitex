4d44e1e6fc4c30a113d74827d27a9588d0df5d5f
Kostya-Bog <59747320+Kostya-Bog@users.noreply.github.com>
2021-01-23 01:41:20 +0200

    Added Foreman.cs

---
+++ TestTask/Models/Foreman.cs
@@ -0,0 +1,23 @@
+﻿namespace TestTask.Models
+{
+    using Abstractions;
+    using System;
+
+    public class Foreman : Employee
+    {
+        public Foreman(string fullName, double experience)
+            : base(fullName, experience, Enums.Position.Foremans)
+        {
+        }
+
+        public override void Work()
+        {
+            Console.WriteLine("Purchase of materials");
+        }
+
+        public void ValidationCheck()
+        {
+            Console.WriteLine("I have performed a validation check");
+        }
+    }
+}
