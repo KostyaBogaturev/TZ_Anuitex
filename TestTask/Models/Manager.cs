6098cdf983bb22a303b49e285800826712368ac5
Kostya-Bog <59747320+Kostya-Bog@users.noreply.github.com>
2021-01-23 01:37:03 +0200

    Added Manager.cs

---
+++ TestTask/Models/Manager.cs
@@ -0,0 +1,23 @@
+﻿namespace TestTask.Models
+{
+    using Abstractions;
+    using System;
+
+    public class Manager : Employee
+    {
+        public Manager(string fullName, double experience)
+            : base(fullName, experience, Enums.Position.Managers)
+        {
+        }
+
+        public override void Work()
+        {
+            Console.WriteLine("Taking orders");
+        }
+
+        public void GiveTask()
+        {
+            Console.WriteLine("I issued a task");
+        }
+    }
+}
