ee432fda00a96ef94e9dfd1af7a138dd92076701
Kostya-Bog <59747320+Kostya-Bog@users.noreply.github.com>
2021-01-24 01:13:59 +0200

    Added LinqExtention class

---
+++ TestTask/Helper/LinqExtention.cs
@@ -0,0 +1,22 @@
+﻿namespace TestTask.Helper
+{
+    using Abstractions;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public static class LinqExtention
+    {
+        public static bool Includ(this List<Employee> sourse, string fullName)
+        {
+            var list = new List<string>();
+            foreach (var item in sourse)
+            {
+                list.Add(item.FullName);
+            }
+
+            // I used query syntax because List has the Method "Contains"
+            // in this case we didn't use Linq
+            return ((from n in list select n).Contains(fullName));
+        }
+    }
+}
