546b686f3811e92e26ed506a9c0be03f1952af75
Kostya-Bog <59747320+Kostya-Bog@users.noreply.github.com>
2021-01-23 02:50:54 +0200

    Added class Firm.cs

---
+++ TestTask/Models/Firm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestTask.Models
+{
+    using Abstractions;
+    public class Firm
+    {
+        private List<Employee> list = new List<Employee>();
+
+        public static Firm operator +(Firm firm, Employee item)
+        {
+            firm.list.Add(item);
+            return firm;
+        }
+
+        public static Firm operator -(Firm firm, Employee item)
+        {
+            firm.list.Remove(item);
+            return firm;
+        }
+    }
+}
